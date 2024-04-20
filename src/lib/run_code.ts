f03086761840c3d506bdc10d9f78e9f7f5a0f546
Mokshit06 <50412128+Mokshit06@users.noreply.github.com>
2024-04-21 02:23:05 +0530

    remove hard-coded error

--- src/lib/run_code.ts
+++ src/lib/run_code.ts
@@ -1,7 +1,3 @@
 export default function run_code(str: string) {
-  if (str.charCodeAt(0) === 101) {
-    throw new Error('SOME_ERROR_MESSAGE');
-  } else {
-    console.log('hello');
-  }
+  console.log((str as any).a.b);
 }
